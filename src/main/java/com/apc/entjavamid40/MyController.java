239ae37f6bf67e99358fe2fefc1a5ea306bf4ad2
Gabrielle Jonathan Juanillo <gcjuanillo@student.apc.edu.ph>
2024-09-24 08:39:29 +0800

    Reconfigure Controller. Added JSON Builder and UnitConversionService

--- src/main/java/com/apc/entjavamid40/MyController.java
+++ src/main/java/com/apc/entjavamid40/MyController.java
@@ -1,16 +1,27 @@
 package com.apc.entjavamid40;
 
 
+import com.apc.entjavamid40.builder.JSONBuilder;
+import com.apc.entjavamid40.service.UnitConversionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("convert")
 public class MyController {
+
+    @Autowired
+    private UnitConversionService conversionService;
+
+    @Autowired
+    private JSONBuilder jsonBuilder;
 
     /*
     * /convert/1/in/cm
@@ -18,11 +29,18 @@ public class MyController {
     * out put should be found in sample_out.json
     * */
 
-    @GetMapping("/{value}/{unit1}/{unit2}/")
-    public String convert() {
-        return "";
-    }
-
     List<String> allowedUnits = Arrays.asList("mm","cm","in","m","ft");
+
+    @GetMapping("/{value}/{unit1}/{unit2}")
+    public Map<String, Object> convert(@PathVariable double value,
+                                       @PathVariable String unit1,
+                                       @PathVariable String unit2) {
+        if (!allowedUnits.contains(unit1) || !allowedUnits.contains(unit2)) {
+            throw new IllegalArgumentException("Invalid unit. Allowed units are: " + allowedUnits);
+        }
+
+        double convertedValue = conversionService.convert(unit1, unit2, value);
+        return jsonBuilder.buildResponse(unit1, value, unit2, convertedValue);
+    }
 
 }
